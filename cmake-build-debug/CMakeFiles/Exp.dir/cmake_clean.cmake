8b5970a670cd32cee3dc55aff12dcc76e05ffa65
bugducky <1404346945@qq.com>
2019-12-18 14:37:06 +0800

    0.1 0.2

---
+++ cmake-build-debug/CMakeFiles/Exp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exp.dir/src/glad.c.obj"
+  "CMakeFiles/Exp.dir/src/main.cpp.obj"
+  "Exp.exe"
+  "Exp.exe.manifest"
+  "Exp.pdb"
+  "libExp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Exp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
